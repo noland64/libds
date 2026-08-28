7fd6bd85c663261b79d57dc677c53fb7036413e5
Daniel Nolan <danielnolan0904@gmail.com>
2026-08-28 11:34:01 +0100

    Remove redundant comments

--- include/queue.h
+++ include/queue.h
@@ -4,32 +4,26 @@
 typedef struct Queue Queue;
 
 
-// queueCreate(void)
 // Create and allocate memory for a new (FIFO) Queue
 // Return newly created Queue, or NULL in the event of error
 Queue* queueCreate(void);
 
-// queueDestroy(Queue* queue)
 // Destroy (FIFO) queue and free allocated memory
 // Return success/error code
 int queueDestroy(Queue* queue);
 
-// queueEnqueue(Queue* queue, int val)
 // Add val to the back of the (FIFO) queue
 // Return success/error code
 int queueEnqueue(Queue* queue, int val);
 
-// queueDequeue(Queue* queue)
 // Retrieve and remove the value at the front of the (FIFO) queue
 // Return the value, or an error code
 int queueDequeue(Queue* queue);
 
-// queuePeek(Queue* queue)
 // Retrieve the value at the front of the FIFO queue
 // Return the value, or an error code
 int queuePeek(Queue* queue);
 
-// queueSize(Queue* queue)
 // Retrieve the size of the FIFO queue
 // Return the value, or an error code
 int queueSize(Queue* queue);
